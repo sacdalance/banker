c0e2b7f68e360c17dc15a423a43da191fa90c70f
Lance Sacdalan <63360390+sacdalance@users.noreply.github.com>
2025-05-01 21:34:54 +0800

    banker init

---
+++ banker.c
@@ -0,0 +1,33 @@
+	#include <stdio.h>
+	
+	#define P 5 // row
+	#define R 5 // column
+	
+	int main() {
+		
+		int available[P];
+		int max[P][R] = {    
+			{1, 2, 3, 4, 5},
+    		{0, 1, 2, 3, 4},
+    		{0, 0, 0, 0, 0},
+    		{0, 0, 0, 0, 0},
+    		{0, 0, 0, 0, 0}
+    	};
+		int allocation[P][R] = {0};
+		int need[P][R] = {0}; 
+		
+		int i = 0;
+		int j = 0;
+			
+	    for (i = 0; i < P; i++) {
+			for (j = 0; j < R; j++) {
+				printf("%d", max[i][j]);
+			}
+			printf("\n");
+		}
+		return 0;
+	}
+	
+	void safetyAlgorithm() {
+	}
+
